7789bc85e4856948853d04dfdfed3579cefa2f3f
Azanov, Ivan <ivan.azanov@alibaba-inc.com>
2023-12-28 13:54:31 +0300

    Added ignore

--- liza_aux/common_util.py
+++ liza_aux/common_util.py
@@ -1,19 +1,17 @@
-import logging
-import time
+def display_photo(
+    link: str,
+    title: str = None,
+) -> None:
+    from PIL import Image
+    from io import BytesIO
+    from urllib.request import urlopen
+    import matplotlib.pyplot as plt
 
-import pandas as pd
-
-logger = logging.getLogger(__name__)
-
-
-def elapsed_from(start_time: float) -> int:
-    current_time = time.time()
-    elapsed = int(current_time - start_time)
-    return elapsed
-
-
-def remove_empty_columns(input_df: pd.DataFrame) -> pd.DataFrame:
-    non_empty_columns = (~input_df.isna()).sum()
-    non_empty_columns = non_empty_columns[non_empty_columns > 0]
-    output_df = input_df[non_empty_columns.index.to_list()]
-    return output_df
+    f = urlopen(link)
+    b = BytesIO(f.read())
+    i = Image.open(b)
+    _, ax = plt.subplots(figsize=(8,8))
+    ax.axis('off')
+    ax.imshow(i)
+    if title is not None:
+        ax.set_title(title, fontsize=20)
